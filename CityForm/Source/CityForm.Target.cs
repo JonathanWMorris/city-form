7c562c67397ed83432c26dc12c9e0f66bf64045c
Jonathan W. Morris <jonathan_wellington@icloud.com>
2026-07-30 10:05:38 -0700

    Add the Core-only CitySimulation module

--- CityForm/Source/CityForm.Target.cs
+++ CityForm/Source/CityForm.Target.cs
@@ -10,6 +10,6 @@ public class CityFormTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V7;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
-		ExtraModuleNames.Add("CityForm");
+		ExtraModuleNames.AddRange(new string[] { "CityForm", "CitySimulation" });
 	}
 }
--- CityForm/Source/CityFormEditor.Target.cs
+++ CityForm/Source/CityFormEditor.Target.cs
@@ -10,6 +10,6 @@ public class CityFormEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V7;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
-		ExtraModuleNames.Add("CityForm");
+		ExtraModuleNames.AddRange(new string[] { "CityForm", "CitySimulation" });
 	}
 }
